2883d0056e304ac264543a0abbe0404236e03481
Tapoch <zvp_97@mail.ru>
2021-08-11 16:25:53 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import transform from './utils';
+
 
 function App() {
+  const [text, setText] = useState('test');
+
+  useEffect(() => {
+
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <textarea value={text} onChange={(e) => setText(e.target.value)}></textarea>
+        <p>{transform(text)}</p>
       </header>
     </div>
   );
